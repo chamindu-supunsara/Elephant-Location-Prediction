f8fbcc4a9885fd1eade1c85120c1ba43e8ba4000
Chamindu Supunsara <chamindu.s@sits.lk>
2023-09-22 19:23:31 +0530

    Issue Fix New Angular npm install

---
+++ Web Application/ELPS/ELPS.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using ELPS.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELPS.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpGet("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
