596c9dee66fd35d246a5c82ed23c60d164825317
Chamindu Supunsara <chamindu.s@sits.lk>
2023-09-23 13:50:53 +0530

    Register Form Design and Validation

--- Web Application/ELPS/ELPS.Api/ClientApp/src/app/app.module.ts
+++ Web Application/ELPS/ELPS.Api/ClientApp/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './views/pages/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Web Application/ELPS/ELPS.Api/ClientApp/src/datamodels/dataentity.ts
@@ -0,0 +1,11 @@
+export class registerFormPaper {
+    Email: String="";
+    Firstname: String="";
+    LastName: String="";
+    MobileNumber: String="";
+    NIC: String="";
+    EmpNo: String="";
+    Gender: String="";
+    Password: String="";
+    ComPassword: String="";
+  }
