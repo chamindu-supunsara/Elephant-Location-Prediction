e6573a674c0694e85058906bb3ec37afc7e8b591
Chamindu Supunsara <chamindu.s@sits.lk>
2023-09-25 00:02:15 +0530

    API Connecting Done

---
+++ Web Application/ELPS/ELPS.Api/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELPS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult Create() 
+        {
+            return Ok("Success from Create Method");
+        }
+    }
+}
--- Web Application/ELPS/ELPS.Api/Program.cs
+++ Web Application/ELPS/ELPS.Api/Program.cs
@@ -7,6 +7,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +31,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowOrigin");
 
 app.Run();
